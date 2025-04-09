3bf602440c89870dd8d707dab8b2b847a3b81f95
Rohitprasad004 <97969388+Rohitprasad004@users.noreply.github.com>
2025-04-09 15:16:18 +0530

    app.js

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-//? Definición de constantes de los valores, numeros y operadores.
-const displayValorAnterior = document.getElementById('valor-anterior');
-const displayValorActual = document.getElementById('valor-actual');
-const btnNum = document.querySelectorAll('.numero');
-const btnOperators = document.querySelectorAll('.operador');
+const display = document.getElementById("display");
 
-//? metodo que muestra el valor actual y anterior
-const display = new Display(displayValorAnterior, displayValorActual);
+function append(value) {
+  display.value += value;
+}
 
-//! Agrega los numeros al darle click
-btnNum.forEach(boton =>{
-    boton.addEventListener('click', () =>{
-        display.agregarNumero(boton.innerHTML);
-    })
-});
+function clearDisplay() {
+  display.value = "";
+}
 
-//* Agrega los datos de operadores
-btnOperators.forEach(boton =>{
-    boton.addEventListener('click', () => display.computar(boton.value))
-});
+function deleteLast() {
+  display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = "Error";
+  }
+}
